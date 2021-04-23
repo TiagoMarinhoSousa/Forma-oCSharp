3b85a7df92219aceaeb15aa84f995b492f65ffb9
Tiago Marinho Sousa <54487262+TiagoMarinhoSousa@users.noreply.github.com>
2021-04-23 12:39:12 -0300

    Add files via upload

---
+++ 01-AprendendoCSharp/N10-CalculaPoupanca/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 10 - Calcula poupança");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            //0.36% = 0.0036
+            while (contadorMes <=12)
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " mes(es), você terá R$" + valorInvestido);
+                //contadorMes = contadorMes + 1;
+                //contadorMes += 1;
+                contadorMes++;             
+            }
+            
+
+            Console.WriteLine("A execuação terminou. Pressione enter. . .");
+            Console.ReadLine();
+        }
+    }
+}
