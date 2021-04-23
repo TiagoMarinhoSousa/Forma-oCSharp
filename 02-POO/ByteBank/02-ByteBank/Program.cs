82f171d4e2f9591f5db09579dbce3a5e75dba3c3
Tiago Marinho Sousa <54487262+TiagoMarinhoSousa@users.noreply.github.com>
2021-04-23 12:42:14 -0300

    Add files via upload

---
+++ 02-POO/ByteBank/02-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //o valor atribuido no objeto se sobrepõe ao da classe
+            ContaCorrente conta = new ContaCorrente();
+            //na classe o saldo está 100, se não atribuir valor esse será o valor default
+            conta.saldo = 200;
+
+            Console.WriteLine(conta.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
