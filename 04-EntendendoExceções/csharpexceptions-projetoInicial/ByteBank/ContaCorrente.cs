732b19cd8d73185bd01d67dcc509521c6f7bcc43
Tiago Marinho Sousa <54487262+TiagoMarinhoSousa@users.noreply.github.com>
2021-05-10 10:05:57 -0300

    Add files via upload

---
+++ 04-EntendendoExceções/csharpexceptions-projetoInicial/ByteBank/ContaCorrente.cs
@@ -0,0 +1,105 @@
+﻿// using _05_ByteBank;
+
+using System;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public static double TaxaOperacao { get; private set; }
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+        public Cliente Titular { get; set; }
+
+        public int ContadorSaquesNaoPermitidos { get; private set; }
+        public int ContadorTransferenciasNaoPermitidas { get; private set; }
+
+        public int Agencia { get; }
+        public int Numero { get; }
+
+        private double _saldo = 100;
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        //construtor
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+
+
+            if (agencia <= 0)
+            {
+                throw new ArgumentException("O argumento agência deve ser maior que 0.", nameof(agencia));
+            }
+
+            if (numero <= 0)
+            {
+                throw new ArgumentException("O argumento agência deve ser maior que 0.", nameof(numero));
+            }              
+           
+
+            TotalDeContasCriadas++;
+
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+        }
+
+
+        public void Sacar(double valor)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor inválido para saque.", nameof(valor));
+            }
+            if (_saldo < valor)
+            {
+                ContadorSaquesNaoPermitidos++;
+                throw new SaldoInsuficienteException(Saldo, valor);
+            }
+
+            _saldo -= valor;
+           
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor inválido para a transferência.", nameof(valor));
+            }
+
+            try
+            {
+                Sacar(valor);
+            }
+            catch(SaldoInsuficienteException ex)
+            {
+                ContadorTransferenciasNaoPermitidas++;
+                throw new OperacaoFinanceiraException("Operação não realizada.", ex);
+            }
+            contaDestino.Depositar(valor);
+            
+        }
+    }
+}
