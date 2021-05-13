955b9cd73a36eb813498bd1a6932e162902c93e0
Tiago Marinho Sousa <54487262+TiagoMarinhoSousa@users.noreply.github.com>
2021-05-13 11:28:54 -0300

    Add files via upload

---
+++ 05-BibliotecasDLLDocumentacaoNuget/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            DateTime dataFimPagamento = new DateTime(2021, 07, 05);
+            DateTime dataCorrente = DateTime.Now;
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+
+            Console.WriteLine(dataCorrente);
+            Console.WriteLine(dataFimPagamento);
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            Console.WriteLine(mensagem);
+
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
