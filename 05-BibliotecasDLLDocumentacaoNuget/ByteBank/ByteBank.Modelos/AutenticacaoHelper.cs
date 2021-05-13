117b8072b5481717c737801e6d88f4b9b730177c
Tiago Marinho Sousa <54487262+TiagoMarinhoSousa@users.noreply.github.com>
2021-05-13 11:16:02 -0300

    Add files via upload

---
+++ 05-BibliotecasDLLDocumentacaoNuget/ByteBank/ByteBank.Modelos/AutenticacaoHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    //se não declarar o modificador de acesso, ela será uma classe interna, como abaixo
+    //ela só faz parte da biblioteca --ByteBank.Modelos
+    internal class AutenticacaoHelper
+    {
+        public bool CompararSenhas(string senhaVerdadeira, string senhaTentativa)
+        {
+            return senhaVerdadeira == senhaTentativa;
+        }
+    }
+}
